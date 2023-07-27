931c28eadb404e4e056ab3f20c95a6c0da5dc6a5
garjeatharv <87515825+garjeatharv@users.noreply.github.com>
2023-07-27 12:06:03 +0530

    Scrapper class updated

--- AskWordTkinter.py
+++ AskWordTkinter.py
@@ -8,7 +8,8 @@ import customtkinter
 
 
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+customtkinter.set_default_c
+olor_theme("green")
 
 root = customtkinter.CTk()
 
@@ -40,27 +41,44 @@ def submit():
 
     resp = requests.get(url)
 
-    soup=BeautifulSoup(resp.text,'html.parser')    
-  
+    soup = BeautifulSoup(resp.text,'html.parser')    
+   
     # # l is the list which contains all the meaning 
     
-    l=soup.find("span",{"class":"one-click-content css-nnyc96 e1q3nk1v1"})
+    # ESah86zaufmd2_YPdZtq
+    l = soup.find("div",{"class":"ESah86zaufmd2_YPdZtq"})
+    
+    
     # #now we want to print only the text part of the anchor.
     # #find all the elements of a, i.e anchor tag
     
     material = ""
 
+# updated scraped class below 
+##<div class="_bzA3f8_vqmJSIKsgOar"><ol start="1"><li><div class="ESah86zaufmd2_YPdZtq" data-type="word-definition-content"><p>a hard, brittle, noncrystalline, more or less transparent substance produced by fusion, usually consisting of mutually dissolved silica and silicates that also contain soda and lime, as in the ordinary variety used for windows and bottles.</p></div><aside class="jeTO7vsIKonI6Z05WfmT ehTSU7DreSCZJDrYAQxS" data-type="ad-vertical"><div class="fHACXxic9xvQeSNITiwH" id="dcomMobileSERPDisplayTopAd-300x250"></div></aside></li><li><div class="ESah86zaufmd2_YPdZtq" data-type="word-definition-content"><p>any artificial or natural noncrystalline and transparent hard substance, such as fused borax, obsidian, or the like.</p></div></li></ol></div>
+    
+    
     if l is not None:
         for i in l:
              material = material + i.text
 
+    material_length = len(material)
+
+    clean_material = ""
+    for i in range(1,material_length-1):
+        clean_material = clean_material + material[i]
+
+    print(material_length)
+    print(clean_material)
+
+
     # #show meaning in messagebox
     if l is None:
-        material = "Sorry, couldn't find your word😥😓"
+        clean_material = "Sorry, couldn't find your word😥😓"
 
-    messagebox.showinfo("meaning of " + word , material)
+    messagebox.showinfo("meaning of " + word , clean_material)
     
-    # print(word)
+    print(word)
 
 
 button = customtkinter.CTkButton(master = root , text = 'Search!' , command = submit).place(x = 180 , y = 200)
